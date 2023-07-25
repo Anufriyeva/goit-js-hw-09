a071a1e3f3fd329df4c9bf3e48b64b308d029421
Anufriyeva <126349829+Anufriyeva@users.noreply.github.com>
2023-07-25 11:34:07 +0000

    Deploying to gh-pages from @ Anufriyeva/goit-js-hw-09@99c808b41a90ca001d3093d214580535a3d065b7 🚀

---
+++ 01-color-switcher.3175e380.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let a;t.addEventListener("click",(()=>{t.disabled=!0,t.style.opacity=.5,e.disabled=!1,a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3)})),e.addEventListener("click",(()=>{clearInterval(a),t.disabled=!1,t.style.opacity=1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.3175e380.js.map
